70c79e51b6f1cabcb354c5f6fdd145dfb9bb4e6b
Rudranil Goswami <nilrudra068@gmail.com>
2026-02-23 09:27:07 +0530

    Track media files and force media serving in production

--- SPOTIFY/mysite/urls.py
+++ SPOTIFY/mysite/urls.py
@@ -4,9 +4,12 @@ from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.contrib.staticfiles.urls import static 
+from django.urls import re_path
+from django.views.static import serve
+
 urlpatterns = [
     path("",include("music.urls")),
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 urlpatterns+=staticfiles_urlpatterns()
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
