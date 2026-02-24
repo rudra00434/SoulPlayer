83783af48b1a94ad8fb0aaf9e4e4075ae228e868
Rudranil Goswami <nilrudra068@gmail.com>
2026-02-25 03:19:50 +0530

    user profile table added

--- SPOTIFY/music/models.py
+++ SPOTIFY/music/models.py
@@ -48,10 +48,12 @@ class UserProfile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+    else:
+        # Check if profile exists before saving to avoid RelatedObjectDoesNotExist errors
+        if hasattr(instance, 'userprofile'):
+            instance.userprofile.save()
+        else:
+            UserProfile.objects.create(user=instance)
